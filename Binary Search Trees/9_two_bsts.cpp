f183a2a2c6b5dfadbecafb5507c43ba02e2d529c
Prabhat GP <prabhat.ai20@bmsce.ac.in>
2023-10-14 22:25:48 +0530

    bst-6

---
+++ Binary Search Trees/9_two_bsts.cpp
@@ -0,0 +1,67 @@
+// 1305. All Elements in Two Binary Search Tree 
+void inorder(TreeNode* root, vector<int>& ans) {
+    if(root == nullptr) return;
+
+    inorder(root->left, ans);
+    ans.push_back(root->val);
+    inorder(root->right, ans);
+}
+
+vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+    vector<int> temp1;
+    vector<int> temp2;
+
+    inorder(root1, temp1);
+    inorder(root2, temp2);
+    vector<int> res;
+    int i = 0;
+    int j = 0;
+    while(i < temp1.size() || j < temp2.size()) {
+        if(i < temp1.size() && (j == temp2.size() || temp1[i] < temp2[j])) {
+            res.push_back(temp1[i]);
+            i++;
+        } else if(j < temp2.size()) {
+            res.push_back(temp2[j]);
+            j++;
+        }
+    }
+    return res;
+}
+
+
+
+
+// Find Common Nodes in two BSTs
+void inorder(TreeNode* root, vector<int>& ans) {
+    if(root == nullptr) return;
+
+    inorder(root->left, ans);
+    ans.push_back(root->val);
+    inorder(root->right, ans);
+}
+
+vector<int> commonNodes(TreeNode* root1, TreeNode* root2) {
+    vector<int> temp1;
+    vector<int> temp2;
+
+    inorder(root1, temp1);
+    inorder(root2, temp2);
+    vector<int> res;
+    int i = 0;
+    int j = 0;
+    while(i < temp1.size() && j < temp2.size()) {
+        if(temp1[i] < temp2[j])
+            i++;
+        else if(temp1[i] > temp2[j])
+            j++;
+        else {
+            res.push_back(temp1[i]);
+            i++;
+            j++;
+        }
+    }
+}
+
+
+
+// 1382. Balance a Binary Search Tree 
