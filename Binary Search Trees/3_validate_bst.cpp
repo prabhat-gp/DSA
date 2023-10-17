2d20b3ec341cf958d49160e927bceeb81f65fd6e
Prabhat GP <prabhat.ai20@bmsce.ac.in>
2023-10-17 20:06:37 +0530

    bst-7

--- Binary Search Trees/3_validate_bst.cpp
+++ Binary Search Trees/3_validate_bst.cpp
@@ -38,3 +38,46 @@ bool isValidBST(TreeNode* root) {
 // LCA in BST 
 // Find common nodes in two BSTs 
 // 501. Find Mode in Binary Search Tree 
+
+// 1361. Validate Binary Tree Nodes
+#include <vector>
+#include <deque>
+#include <unordered_set>
+
+bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+    int root = 0;
+    unordered_set<int> childrenNodes;
+
+    for(int i = 0; i < n; i++) {
+        if(leftChild[i] != -1)
+            childrenNodes.insert(leftChild[i]);
+        if(rightChild[i] != -1)
+            childrenNodes.insert(rightChild[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(childrenNodes.find(i) == childrenNodes.end())
+            root = i;
+    }
+
+    unordered_set<int> visited;
+    deque<int> dq;
+    dq.push_back(root);
+
+    while(!dq.empty()) {
+        int curr = dq.front();
+        dq.pop_front();
+
+        if(visited.find(curr) != visited.end())
+            return false;
+        
+        visited.insert(curr);
+
+        if(leftChild[curr] != -1)
+            dq.push_back(leftChild[curr]);
+        if(rightChild[curr] != -1)
+            dq.push_back(rightChild[curr]);
+    }
+
+    return visited.size() == n;
+}
